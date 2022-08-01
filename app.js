3d8efc68dc2e874530cd67d74716fc545763ec69
Shii_039 <enshikuku39@gmail.com>
2022-08-01 14:19:20 +0300

    updates

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ app.set('view engine', 'ejs')
 // specify where to source for static files
 app.use(express.static('public'))
 
+const items = [
+    {id: 1, name: 'mangoes'},
+    {id: 2, name: 'bananas'},
+    {id: 3, name: 'pencil'},
+    {id: 4, name: 'pen'},
+    {id: 5, name: 'notebook'}
+]
+
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', {
+        data: 'this is data from server',
+        items: items
+    })
 })
+// 404  error
 
+app.get('*', (req, res) => {
+    res.render('pnf')
+})
 app.listen(3000)
